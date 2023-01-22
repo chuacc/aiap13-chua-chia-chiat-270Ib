3b8ff13fc17dd2afb5679886172e6d9fa5179cd2
chuacc <chuacc@gmail.com>
2023-01-22 16:37:01 +0800

    first commit

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir data
+wget https://techassessment.blob.core.windows.net/aiap13-assessment-data/failure.db -O ./data/failure.db
+python ./src/main.py --model RandomForest
+python ./src/main.py --model LogisticRegression
+python ./src/main.py --model SVC
+python ./src/main.py --model DecisionTree
+python ./src/main.py --model GradientBoost
